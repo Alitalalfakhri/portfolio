2050a41de07feacc22fd30dbfeeb734848ef94f5
Alitalalfakhri <ali.talalfakhri09@gmail.com>
2025-07-29 17:49:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -129,5 +129,5 @@ document.querySelector('.web-station-project').addEventListener('click' , () => 
 })
 
 document.querySelector('.book-haven').addEventListener('click' , () => {
-  window.location.href = 'https://sumbook-front.vercel.app/'
+  window.location.href = 'https://sumbook-client.vercel.app/'
 })
